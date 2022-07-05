7d9eafe621e6e838c46a0a143b2e9024a848d7f7
Crhistian de Oliveira <crhistiandeoliveira@gmail.com>
2022-07-04 23:22:46 -0300

    add lógica no appbar

--- src/Components/AppBar.tsx
+++ src/Components/AppBar.tsx
@@ -4,26 +4,34 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import CustomLink from './CustomLink';
 
 export default function CustomAppBar() {
+
+
+  const location = useLocation()
+  console.log(location.pathname);
+  
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <CustomLink isAppBar to='' >
-              <ArrowBackIcon />
-            </CustomLink>
-          </IconButton>
-          <Typography textAlign='center' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {location.pathname !== "/" &&
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <CustomLink isAppBar to='' >
+                  <ArrowBackIcon />
+                </CustomLink>
+              </IconButton>
+           }
+          <Typography variant="h6" component="div" sx={{ position:'absolute', left: '50%', transform: "translateX(-50%)" }}>
             Meu jogo da memória
           </Typography>
         </Toolbar>
