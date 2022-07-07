ab255f9e40b58c3ded36b3cc021f1159d8c6fb2a
Crhistian de Oliveira <crhistiandeoliveira@gmail.com>
2022-07-06 22:47:49 -0300

    add switch

--- src/Components/TabelaSimples.tsx
+++ src/Components/TabelaSimples.tsx
@@ -6,6 +6,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { FormControlLabel, FormGroup, Switch, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 type Row = {
     nome: string;
@@ -33,15 +35,45 @@ const localStorageRows: Row[] = [
 
 // window.localStorage.setItem('ranking', JSON.stringify(localStorageRows))
 
-function comparar(a: Row, b: Row) {
-    return a.jogadas - b.jogadas
-}
+
 
 export default function TabelaSimples() {
+
+    function compararPorJogadas(a: Row, b: Row) {
+        return a.jogadas - b.jogadas
+    }
+    
+    function compararPorTempo(a: Row, b: Row) {
+        return Number(a.tempo) - Number(b.tempo)
+    }
+
+    const [checked, setChecked] = React.useState(true);
+
     let rows:Row[] = JSON.parse(window.localStorage.getItem('ranking') || '[]')
-    rows.sort(comparar)
+    rows.sort(checked ? compararPorJogadas : compararPorTempo )
     
   return (
+    <>
+
+    {/* <FormGroup>
+        <FormControlLabel control={<Switch
+          checked={checked}
+          onChange={(e)=>setChecked(e.target.checked)}
+          />} label="Ordenar classificação" />
+    </FormGroup> */}
+
+    <Box display='flex' flexDirection='column' alignItems='center'>
+        <Typography>Ordenar classificação</Typography>
+        <Box display='flex' alignItems='center'>
+            <Typography>tempo</Typography>
+        <Switch
+          checked={checked}
+          onChange={(e)=>setChecked(e.target.checked)}
+          />
+          <Typography>jogadas</Typography>
+        </Box>
+    </Box>
+    
     <TableContainer component={Paper} >
       <Table  size="small">
         <TableHead>
@@ -66,12 +98,13 @@ export default function TabelaSimples() {
               </TableCell>
               <TableCell align="right">{row.nome}</TableCell>
               <TableCell align="right">{row.jogadas}</TableCell>
-              <TableCell align="right">{row.tempo}</TableCell>
+              <TableCell align="right">{Number(row.tempo) > 59 ? parseInt(`${(Number(row.tempo)/60)}`)+":"+`${Number(row.tempo)%60}` : row.tempo}</TableCell>
             </TableRow>
           )
           })}
         </TableBody>
       </Table>
     </TableContainer>
+    </>
   );
 }
